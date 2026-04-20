0c9ad75ba5a9f4b4c9f5634deb09a07d7dd2f311
Miguel <miguel.mglez@gmail.com>
2026-04-20 15:14:20 +0200

    added fixes for Still alive situations

--- app/src/main/java/com/mmg/manahub/feature/game/GameViewModel.kt
+++ app/src/main/java/com/mmg/manahub/feature/game/GameViewModel.kt
@@ -271,7 +271,7 @@ class GameViewModel @Inject constructor(
     fun checkPendingDefeat() {
         _uiState.update { s ->
             s.copy(players = s.players.map { p ->
-                if (p.defeated || p.pendingDefeat) p
+                if (p.defeated || p.pendingDefeat || p.isSurviving) p
                 else if (shouldEliminate(p, s.mode)) p.copy(pendingDefeat = true)
                 else p
             })
@@ -283,7 +283,7 @@ class GameViewModel @Inject constructor(
     fun confirmDefeat(playerId: Int) {
         _uiState.update { s ->
             s.copy(players = s.players.map { p ->
-                if (p.id == playerId) p.copy(defeated = true, pendingDefeat = false) else p
+                if (p.id == playerId) p.copy(defeated = true, pendingDefeat = false, isSurviving = false) else p
             })
         }
         checkWinner()
@@ -293,7 +293,7 @@ class GameViewModel @Inject constructor(
     fun revokeDefeat(playerId: Int) {
         _uiState.update { s ->
             s.copy(players = s.players.map { p ->
-                if (p.id == playerId) p.copy(pendingDefeat = false, defeated = false) else p
+                if (p.id == playerId) p.copy(pendingDefeat = false, isSurviving = true, defeated = false) else p
             })
         }
     }
